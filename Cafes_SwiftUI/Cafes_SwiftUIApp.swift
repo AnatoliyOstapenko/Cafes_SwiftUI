82a07c0968ccefb5aaaf0653c65efed9bb06efca
Anatoliy Ostapenko <anatoliy.ostapenko@gmail.com>
2023-04-21 13:08:29 +0300

    Initial Commit

---
+++ Cafes_SwiftUI/Cafes_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cafes_SwiftUIApp.swift
+//  Cafes_SwiftUI
+//
+//  Created by Anatoliy Ostapenko on 21.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct Cafes_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
